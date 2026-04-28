e05c609355d6db75cf03471d9b8493e7293d9180
Aayash <dangaayash@gmail.com>
2026-04-28 12:50:29 +0545

    update

--- src/routes/leadRoutes.js
+++ src/routes/leadRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createLead, getLeads, updateLead, deleteLead } from "../controllers/leadController.js";
+import { protect, adminOnly } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", createLead);  
-router.get("/", getLeads);
-router.put("/:id", updateLead);
-router.delete("/:id", deleteLead);
+router.post("/", protect, createLead);  
+router.get("/", protect, getLeads);
+router.put("/:id", protect, updateLead);
+router.delete("/:id", protect, deleteLead);
 
 export default router;
